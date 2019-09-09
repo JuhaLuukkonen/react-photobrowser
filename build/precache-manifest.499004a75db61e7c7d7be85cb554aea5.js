8dee5a76a35569f8c531d9d5fb2292d08fab3446
JuhaLuukkonen <32368550+JuhaLuukkonen@users.noreply.github.com>
2019-09-09 11:28:19 +0300

    upload build folder

---
+++ build/precache-manifest.499004a75db61e7c7d7be85cb554aea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "de0774f5018ea62be4c8",
+    "url": "/static/css/main.8055d60d.chunk.css"
+  },
+  {
+    "revision": "de0774f5018ea62be4c8",
+    "url": "/static/js/main.82042e49.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c4b9edcab17def5d0a9b",
+    "url": "/static/js/2.4dcb8bc3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4e6c0a780bc482ed60d7753f8d6853b0",
+    "url": "/index.html"
+  }
+];
